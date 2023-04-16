69692d4a4868386473a11e82bdca0909af7c7fa8
dimasikBurdin <mityabur.gmail.com>
2023-04-16 15:23:11 +0500

    Init: add base controller, entity, setup db

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  const app: NestExpressApplication = await NestFactory.create(AppModule);
+  const config: ConfigService = app.get(ConfigService);
+  const port: number = config.get('PORT');
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  await app.listen(port, () => {
+    console.log('[WEB]', config.get('BASE_URL'));
+  });
 }
+
 bootstrap();
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import './styles/primary-styles.scss';
+import axios from 'axios';
 
 const App = () => {
+  const sendCreateUser = useCallback(() => {
+    axios.post('http://localhost:3001/user', {
+      name: 'test1',
+      email: 'test1@inbox.ru'
+    })
+  }, [])
   return (
     <div className="App">
       Book app web
+      <button onClick={sendCreateUser}>create user</button>
     </div>
   );
 }

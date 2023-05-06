97602c0c04ab4bd515512735a274eab651295979
dimasikBurdin <mityabur@gmail.com>
2023-05-07 01:16:11 +0500

    Api: docker-compose for api and database

---
+++ backend/init.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS book_mate;
+CREATE TABLE [IF NOT EXISTS] table_name (
+   column1 datatype(length) column_contraint,
+   column2 datatype(length) column_contraint,
+   column3 datatype(length) column_contraint,
+   table_constraints
+);
